5ef98aaee4eafa4738978e6a9e2a7504db34f14b
amyheather <a.heather2@exeter.ac.uk>
2026-05-19 12:17:47 +0100

    fix(codespace): further attempt to fix

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,13 +1,5 @@
-# Source: https://github.com/boettiger-lab/nasa-topst-env-justice (MIT)
-# Comments from https://github.com/AnushaPB/codespacer
-
-# Copies a configuration file to disable authentication on RStudio Server,
-# making it accessible without needing to log in.
-sudo cp /etc/rstudio/disable_auth_rserver.conf /etc/rstudio/rserver.conf
-
-# Adds "USER=rstudio" to the end of the /etc/environment file,
-# setting a system-wide environment variable to specify the default user for RStudio.
-sudo sudo bash -c 'echo "USER=rstudio" >>/etc/environment'
-
-# Start the RStudio Server in the background
-sudo /init &> /dev/null &
+#!/usr/bin/env bash
+set -e
+# Add any postCreate steps here (e.g. renv::restore())
+# RStudio Server is started in postAttachCommand, not here
+sudo bash -c 'echo "USER=rstudio" >> /etc/environment'
